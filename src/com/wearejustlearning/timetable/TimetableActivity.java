e904ba476f54681e8ffe02fb050a5c74a4383447
blahato <blaha.androidak@gmail.com>
2012-02-18 15:31:53 +0100

    new method

--- src/com/wearejustlearning/timetable/TimetableActivity.java
+++ src/com/wearejustlearning/timetable/TimetableActivity.java
@@ -1,6 +1,5 @@
 package com.wearejustlearning.timetable;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,31 +11,28 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class TimetableActivity extends Activity implements OnClickListener {
-    /** Called when the activity is first created. */
-    Button button;
-    TextView label;
-    
+
+	Button button;
+	TextView label;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        button = (Button)findViewById(R.id.button);
+        label = (TextView)findViewById(R.id.textLabel);
         
         button.setOnClickListener(this);
-        button = (Button)findViewById(R.id.button);
         setTime();
-        
-
     }
-    
+
     public void setTime(){
-    	TextView label;
-    	label = (TextView)findViewById(R.id.textLabel);
-    	
         String currentDateTimeString = SimpleDateFormat.getDateInstance().format(new Date());  
         SimpleDateFormat formatter = new SimpleDateFormat("hh:mm");  
         String formattedDateString = formatter.format(currentDateTimeString);
-    	label.setText(formattedDateString);
+        label.setText(formattedDateString);
     }
-
+    
+    
 	public void onClick(View view) {
 		setTime();
 		
