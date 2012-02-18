057c90d516a176a487e26fa76f7bafbe11393ab7
blahato <blaha.androidak@gmail.com>
2012-02-18 14:52:05 +0100

    first activity

--- src/com/wearejustlearning/timetable/DB.java
+++
@@ -1,5 +0,0 @@
-package com.wearejustlearning.timetable;
-
-public class DB {
-
-}
--- src/com/wearejustlearning/timetable/TimetableActivity.java
+++ src/com/wearejustlearning/timetable/TimetableActivity.java
@@ -1,7 +1,12 @@
 package com.wearejustlearning.timetable;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TimetableActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,11 @@ public class TimetableActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        TextView label = (TextView)findViewById(R.id.label);
+        String currentDateTimeString = SimpleDateFormat.getDateInstance().format(new Date());  
+        SimpleDateFormat formatter = new SimpleDateFormat("hh:mm");  
+        String formattedDateString = formatter.format(currentDateTimeString);
+        label.setText(formattedDateString);
     }
 }
